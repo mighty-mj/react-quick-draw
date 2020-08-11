117c358eb46e2e686bb05e56cdd080fb65ca29b2
Matthias Jenni <matthias.jenni@swisscom.com>
2020-08-11 11:12:11 +0200

    Updates

---
+++ precache-manifest.ec2facf64e744c229516f34875febd87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8275e7d6f53bc204afed9b187416e11d",
+    "url": "/react-quick-draw/index.html"
+  },
+  {
+    "revision": "681047e8324cbeef4fbb",
+    "url": "/react-quick-draw/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "240304d1331e327387c6",
+    "url": "/react-quick-draw/static/js/2.d02532ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quick-draw/static/js/2.d02532ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681047e8324cbeef4fbb",
+    "url": "/react-quick-draw/static/js/main.40b0325e.chunk.js"
+  },
+  {
+    "revision": "44e4f7c70d2b89d52ace",
+    "url": "/react-quick-draw/static/js/runtime-main.014e8426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-quick-draw/static/media/logo.5d5d9eef.svg"
+  }
+]);
