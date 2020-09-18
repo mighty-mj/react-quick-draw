7687b1516c693ab413b40d6e701c63a4e8968703
Matthias Jenni <matthias.jenni@swisscom.com>
2020-09-18 14:41:02 +0200

    test the point functionality with the reducer hook

---
+++ src/Points.test.js
@@ -0,0 +1,46 @@
+import React, {useReducer} from 'react';
+import {pointReducer} from "./Points";
+import {mount} from "enzyme";
+
+
+function TestPointReducerComponent() {
+    const [points, dispatch] = useReducer(pointReducer, 0);
+
+    return (<div>
+        <button id="reset" onClick={() => dispatch({type: "reset"})} />
+        <button id="addOne" onClick={() => dispatch({type: "addOne"})} />
+        <button id="addTwo" onClick={() => dispatch({type: "addTwo"})} />
+        <button id="minusOne" onClick={() => dispatch({type: "minusOne"})} />
+        <div id="points">{points}</div>
+    </div>);
+}
+
+describe("Test the Point handling with the reducer component", () => {
+
+    const wrapper = mount(<TestPointReducerComponent />);
+
+    it("initial points = 0", () => {
+        expect(wrapper.find({id: "points"}).text(0));
+    });
+
+    it("add one point", () => {
+        wrapper.find({id: "addOne"}).simulate("click");
+        expect(wrapper.find({id: "points"}).text(1));
+    });
+
+    it("add two points", () => {
+        wrapper.find({id: "addTwo"}).simulate("click");
+        expect(wrapper.find({id: "points"}).text(3));
+    });
+
+    it("minus one point", () => {
+        wrapper.find({id: "minusOne"}).simulate("click");
+        expect(wrapper.find({id: "points"}).text(2));
+    });
+
+    it("areset points", () => {
+        wrapper.find({id: "reset"}).simulate("click");
+        expect(wrapper.find({id: "points"}).text(0));
+    });
+
+});
